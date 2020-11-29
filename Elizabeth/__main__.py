4a11680fa01a4d9c9d5528bb79251a1b6231270f
farseendp <72940408+farseendp@users.noreply.github.com>
2020-11-29 17:35:54 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -40,7 +40,7 @@ from Elizabeth.modules.helper_funcs.alternate import typing_action
 
 PM_START_TEXT = f"""
 `Hi..Welcome` 👀
-`I'm` [ELIZABETH🍀](https://telegra.ph/file/281aaa468756db0769df0.jpg)
+`I'm` [Legend king](https://telegra.ph/file/6f10cba7dc3533f729507.jpg)
 `I'm here to help you manage your groups.. Click Help button to find out more about how to use me to my full potential..`
 """
 
